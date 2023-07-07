286afe7aa7771e8d7157edd381c133b1aed014b5
simon0820s <simon0820s@gmail.com>
2023-07-07 10:58:15 -0500

    view camera and video

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,31 @@ import './App.css'
 
 function App () {
   const videoRef = useRef(null)
+  const photoRef = useRef(null)
+  const [hasPhoto, setHasPhoto] = useState(false)
+  const getVideo = () => {
+    navigator.mediaDevices.getUserMedia({ video: { width: 1920, height: 1080 } }).then(stream => {
+      let video = videoRef.current
+      video.srcObject = stream
+      video.play()
+    })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  useEffect(() => {
+    getVideo()
+  }, [videoRef])
   return (
     <div className="app">
       <div className="camera">
         <video ref={videoRef}></video>
         <button>SNAP!</button>
+      </div>
+      <div className={'result' + (hasPhoto ? 'hasPhoto' : '')}>
+        <canvas ref={photoRef}></canvas>
+        <button>CLOSE</button>
       </div>
     </div>
   )
