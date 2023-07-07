e1d475a1cf749d1bcffc9b5d58328d8ab90c2d00
simon0820s <simon0820s@gmail.com>
2023-07-07 10:40:51 -0500

    create camera container, ref and unuse tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-/* eslint-disable react/react-in-jsx-scope */
+import React, { useRef, useEffect, useState } from 'react'
 import './App.css'
 
 function App () {
+  const videoRef = useRef(null)
   return (
-    <div>
-      app
+    <div className="app">
+      <div className="camera">
+        <video ref={videoRef}></video>
+        <button>SNAP!</button>
+      </div>
     </div>
   )
 }
